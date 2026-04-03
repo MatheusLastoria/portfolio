8eac5d9e43e97188a19499c4dd65c2257d96652c
Matheus Lastoria <matheusviniciusdacruz@hotmail.com>
2026-04-03 03:17:50 -0300

    error

--- obj/Debug/net10.0/portfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b10f701143ec0787c7775c45aa920efdd879ef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b3f685a2d007afd47f521855fed4c2dff10722")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
